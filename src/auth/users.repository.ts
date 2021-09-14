31c02b52b237bd0b284a1f930183ec1349878330
Kater Akeren <akerenkater@gmail.com>
2021-09-14 22:58:58 +0100

    username conflicts

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,3 +1,7 @@
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from '@src/auth/user.entity';
 import { AuthCredentialsDto } from '@src/auth/dto/auth-credentials.dto';
@@ -7,6 +11,13 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('Username already in use!');
+      }
+      throw new InternalServerErrorException();
+    }
   }
 }
